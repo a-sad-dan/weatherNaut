cd4e40164b16500707c6265b9e28950c28404a3a
Dan <danishasad91@gmail.com>
2023-11-20 17:31:10 +0530

    Created Page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-document.querySelector('body').innerHTML = "<H1>This is Working</H1>"
+import './style.css'
+
+// let us first make a call
+
+const queryURL = `https://api.weatherapi.com/v1/forecast.json?key=f06fbaa0fe1945cca9771323231811&q=New delhi&days=3&aqi=yes&alerts=yes`;
+const forecastResponse = await fetch(queryURL, { mode: "cors" });
+
+console.log(forecastResponse);
+
