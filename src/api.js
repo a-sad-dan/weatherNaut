8be653d46331a1caa98d132359b5963df8ac35dc
Dan <danishasad91@gmail.com>
2024-01-31 10:54:55 +0530

    add module

--- src/api.js
+++ src/api.js
@@ -1,4 +1,13 @@
-const queryURL = `https://api.weatherapi.com/v1/forecast.json?key=f06fbaa0fe1945cca9771323231811&q=New delhi&days=3&aqi=yes&alerts=yes`;
-const forecastResponse = await fetch(queryURL, { mode: "cors" });
+async function fetchForecastData(query) {
+    // Starting an api Call here
+    const API_KEY = "f06fbaa0fe1945cca9771323231811";
+    const queryURL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${query}&days=3&aqi=yes&alerts=yes`;
 
-console.log(forecastResponse);
+    const response = await fetch(queryURL, { mode: "cors" });
+    console.log(response);
+    const data = await response.json();
+    console.log("data is ", data);
+    return data;
+}
+
+export default fetchForecastData;
