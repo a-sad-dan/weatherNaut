0b9e729fe9df984408fd26e79101d5b1098cb201
Dan <danishasad91@gmail.com>
2024-03-22 21:39:30 +0530

    added error message

--- src/index.js
+++ src/index.js
@@ -27,6 +27,19 @@ function convertEpochTime(epochTime) {
 }
 
 function updateContent(forecastData) {
+	const mainContent = document.querySelector("#main-content");
+	const errorP = document.querySelector("#error-message");
+
+	// Error handling
+	if (forecastData.error) {
+		errorP.textContent = forecastData.error.message;
+		mainContent.classList.add("hidden");
+		return;
+	}
+
+	errorP.textContent = "";
+	mainContent.classList.remove("hidden");
+	
 	const cityName = document.getElementById("city-name");
 	const countryName = document.getElementById("country-name");
 
@@ -59,7 +72,7 @@ function updateContent(forecastData) {
 	console.log(forecastData.current.feelslike_c);
 
 	const wind = document.getElementById("wind");
-	const windDirection = document.getElementById("wind-direction")
+	const windDirection = document.getElementById("wind-direction");
 	const humidity = document.getElementById("humidity");
 	const uvIndex = document.getElementById("uv-index");
 	const visibility = document.getElementById("visibility");
@@ -70,7 +83,7 @@ function updateContent(forecastData) {
 	const moonPhase = document.getElementById("moon-phase");
 
 	wind.textContent = `${forecastData.current.wind_kph}`;
-	windDirection.textContent = `${forecastData.current.wind_dir}`
+	windDirection.textContent = `${forecastData.current.wind_dir}`;
 	humidity.textContent = forecastData.current.humidity;
 	uvIndex.textContent = forecastData.current.uv;
 	visibility.textContent = forecastData.current.vis_km;
@@ -84,21 +97,20 @@ function updateContent(forecastData) {
 
 	moonPhase.textContent = forecastData.forecast.forecastday[0].astro.moon_phase;
 
-
 	// Rendering the forecast info
 	const forecastsDiv = document.getElementById("forecasts");
 
-
 	for (let i = 0; i < 3; i += 1) {
 		const row = forecastsDiv.querySelectorAll(`[data-day = "${i}"]`);
-		const dateData = convertEpochTime(forecastData.forecast.forecastday[i].date_epoch);
-		row[0].textContent = dateData.day// dayName
-		row[1].textContent = forecastData.forecast.forecastday[i].day.maxtemp_c // max temp
-		row[2].textContent = forecastData.forecast.forecastday[i].day.mintemp_c // min temp
-		row[3].textContent = forecastData.forecast.forecastday[i].day.maxwind_kph // wind
+		const dateData = convertEpochTime(
+			forecastData.forecast.forecastday[i].date_epoch
+		);
+		row[0].textContent = dateData.day; // dayName
+		row[1].textContent = forecastData.forecast.forecastday[i].day.maxtemp_c; // max temp
+		row[2].textContent = forecastData.forecast.forecastday[i].day.mintemp_c; // min temp
+		row[3].textContent = forecastData.forecast.forecastday[i].day.maxwind_kph; // wind
 	}
 }
-
 
 // function to handle form submit
 async function handleFormSubmit(e) {
@@ -106,7 +118,7 @@ async function handleFormSubmit(e) {
 	const searchQuery = document.getElementById("query").value;
 
 	localStorage.setItem("default", searchQuery);
-	console.log("saved query", localStorage.getItem("default"))
+	console.log("saved query", localStorage.getItem("default"));
 	// console.log(searchQuery);
 
 	const data = await fetchForecastData(searchQuery);
@@ -114,15 +126,13 @@ async function handleFormSubmit(e) {
 	updateContent(data);
 }
 
-
 let defaultData;
 if (localStorage.getItem("default")) {
 	const query = localStorage.getItem("default");
 	console.log(query);
 	defaultData = await fetchForecastData(query);
 	updateContent(defaultData);
-}
-else {
+} else {
 	// Setting default as new Delhi if nothing in local storage
 	defaultData = await fetchForecastData("new delhi");
 	updateContent(defaultData);
@@ -134,56 +144,53 @@ function toggleUnits() {
 	const metricSpeed = Array.from(document.querySelectorAll(".metric.speed"));
 	const metricTemp = Array.from(document.querySelectorAll(".metric.temp"));
 
-	const imperialSpeed = Array.from(document.querySelectorAll(".imperial.speed"));
+	const imperialSpeed = Array.from(
+		document.querySelectorAll(".imperial.speed")
+	);
 	const imperialTemp = Array.from(document.querySelectorAll(".imperial.temp"));
 
 	const visibility = document.getElementById("visibility");
 
 	if (metricSpeed.length || metricTemp.length) {
-		metricSpeed.forEach(element => {
+		metricSpeed.forEach((element) => {
 			const value = element.textContent;
 			const converted = Math.round(value * 6.21371192) / 10;
 			element.textContent = converted;
-		})
+		});
 
-		metricTemp.forEach(element => {
+		metricTemp.forEach((element) => {
 			const value = element.textContent;
-			const converted = Math.round(((value * 9 / 5) + 32) * 10) / 10;
+			const converted = Math.round(((value * 9) / 5 + 32) * 10) / 10;
 			element.textContent = converted;
 		});
 
 		visibility.textContent = Math.round(visibility.textContent * 6.21371) / 10;
 	}
 	if (imperialSpeed.length || imperialTemp.length) {
-		imperialSpeed.forEach(element => {
+		imperialSpeed.forEach((element) => {
 			const value = element.textContent;
 			const converted = Math.round(value * 16.0934) / 10;
 			element.textContent = converted;
 		});
 		// f to c
-		imperialTemp.forEach(element => {
+		imperialTemp.forEach((element) => {
 			const value = element.textContent;
-			const converted = Math.round(((value - 32) * 5 / 9) * 10) / 10;
+			const converted = Math.round((((value - 32) * 5) / 9) * 10) / 10;
 			element.textContent = converted;
-		})
+		});
 
-		visibility.textContent = Math.round(visibility.textContent * 16.0934) / 10
+		visibility.textContent = Math.round(visibility.textContent * 16.0934) / 10;
 	}
 
-
-
 	// console.log("data", data);
-	data.forEach(element => {
+	data.forEach((element) => {
 		element.classList.toggle("metric");
 		element.classList.toggle("imperial");
 	});
-
 }
 
-
 const unitBtn = document.getElementById("unit");
-unitBtn.addEventListener("click", toggleUnits)
+unitBtn.addEventListener("click", toggleUnits);
 
 const form = document.querySelector("form");
 form.addEventListener("submit", handleFormSubmit);
-
